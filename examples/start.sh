11a016f63f9e61448eed16bf6c14301872b009b3
blackzid <blackzidd@gmail.com>
2017-07-03 03:08:44 +0800

    Add examples

---
+++ examples/start.sh
@@ -0,0 +1,8 @@
+nohup ./node1.sh 2>>bdata/n1.log &
+sleep 0.3;
+nohup ./node2.sh 2>>bdata/n2.log &
+sleep 0.3;
+nohup ./node3.sh 2>>bdata/n3.log &
+sleep 0.3;
+nohup ./node4.sh 2>>bdata/n4.log &
+sleep 0.3;
